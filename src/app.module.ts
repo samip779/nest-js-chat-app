867cca1d09b54bf6085b22680f9e1c60d5b28e69
samip779 <samipluitel28@gmail.com>
2023-03-05 13:38:07 +0545

    web socket libraries added and new gateway created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfigAsync } from './config/typeorm.config';
+import { SocketModule } from './socket/socket.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { typeOrmConfigAsync } from './config/typeorm.config';
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     AuthModule,
     UsersModule,
+    SocketModule,
   ],
   controllers: [],
   providers: [],
