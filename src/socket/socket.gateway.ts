7f342cd735c660ab03cb433e3d9d025d731cf739
samip779 <samipluitel28@gmail.com>
2023-03-05 17:43:38 +0545

    jwt authorization on socket

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -10,26 +10,33 @@ import { Server } from 'socket.io';
 
 import { AuthService } from 'src/auth/auth.service';
 
+type AuthPayload = {
+  user: any;
+};
+
+export type ServerWithAuth = Server & AuthPayload;
+
 @WebSocketGateway()
 export class SocketGateway implements OnModuleInit {
   constructor(private readonly authService: AuthService) {}
   @WebSocketServer()
-  server: Server;
+  server: ServerWithAuth;
 
   onModuleInit() {
     this.server.on('connection', async (socket) => {
       console.log(socket.id);
       try {
-        const token = socket.handshake.headers.authorization.split('')[1];
+        const token = socket.handshake.headers.authorization;
         if (!token) {
           socket.disconnect();
         }
+        const extractedToken = token.split(' ')[1];
 
-        const user = await this.authService.verifyUser(token);
+        const user = await this.authService.verifyUser(extractedToken);
 
         if (!user) socket.disconnect();
-
-        console.log(user);
+        delete user.password;
+        this.server.user = user;
       } catch (e) {
         console.log(e.message);
         socket.disconnect();
@@ -39,6 +46,6 @@ export class SocketGateway implements OnModuleInit {
 
   @SubscribeMessage('message')
   handleMessage(@MessageBody() body: any) {
-    this.server.emit('onMessage', body);
+    this.server.emit('onMessage', this.server.user.email);
   }
 }
